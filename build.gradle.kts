4438e69e02cd10dd1534d3caaee176c559174038
Artem Smirnov <artem@smirnov.page>
2026-03-20 15:59:45 +0200

    Add Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
 }
 
 buildscript {
     dependencies {
+        classpath(libs.hilt.gradle.plugin)
         classpath(libs.kotlin.gradle.plugin)
         classpath(libs.ksp.gradle.plugin)
     }
