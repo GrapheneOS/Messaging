10fa182e385699600557e2fb10449bdcbfdad5fa
Matvei Plokhov <mpplokho@gmail.com>
2026-04-03 15:42:53 +0200

    Remove old settings implementation

--- src/com/android/messaging/ui/appsettings/redesign/screen/model/SettingsScreenEffect.kt
+++
@@ -1,9 +0,0 @@
-package com.android.messaging.ui.appsettings.redesign.screen.model
-
-internal sealed interface SettingsScreenEffect {
-    data class OpenWirelessAlerts(val subId: Int) : SettingsScreenEffect
-    data object OpenManageDefaultApps : SettingsScreenEffect
-    data object RequestDefaultSmsApp : SettingsScreenEffect
-    data object OpenNotificationSettings : SettingsScreenEffect
-    data object OpenLicenses : SettingsScreenEffect
-}
