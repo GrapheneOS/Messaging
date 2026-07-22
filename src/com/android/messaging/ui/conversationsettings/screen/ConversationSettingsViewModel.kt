1cbf6833e9d651dad4d16275c4de75ad4158c79d
Matvei Plokhov <mpplokho@gmail.com>
2026-07-22 22:58:12 +0200

    Collect screen events through one primitive

--- src/com/android/messaging/ui/conversationsettings/screen/ConversationSettingsViewModel.kt
+++ src/com/android/messaging/ui/conversationsettings/screen/ConversationSettingsViewModel.kt
@@ -22,6 +22,8 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
+internal const val CONVERSATION_SETTINGS_CONVERSATION_ID_ARG = "conversationId"
+
 internal interface ConversationSettingsScreenModel {
     val effects: Flow<Effect>
     val navigationEvents: Flow<NavEvent>
