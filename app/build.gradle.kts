b94c867ba1c5e71364b2f56c284f11d3c9a5ec6f
Matvei Plokhov <mpplokho@gmail.com>
2026-07-30 18:40:12 +0200

    Add material3 adaptive dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -190,6 +190,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
